e2afe745811700f18139ec35247587c877e7d545
Zahir Saiyed <zahir19saiyed@gmail.com>
2023-11-23 23:08:24 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    nextConfig,
-    target: 'serverless',
-    trailingSlash: true,
-}
+module.exports = nextConfig
